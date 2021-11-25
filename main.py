aee3e344a21f4b6175209e1fda5c3145515b530b
briag <briagee101@gmail.com>
2021-11-24 17:19:01 -0800

    change formatting of static a little bit, added api to my page, added the mask on mask off feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # import "packages" from flask
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from templates.aboutus.briaapi import eightball
 # create a Flask instance
 app = Flask(__name__)
 
@@ -35,9 +35,22 @@ def stub():
 def nadira():
     return render_template("aboutus/nadira.html")
 
-@app.route('/bria/')
+@app.route('/bria/', methods=['GET', 'POST'])
 def bria():
     return render_template("aboutus/bria.html")
+
+
+@app.route('/briasapi/', methods=['GET', 'POST'])
+def eightballapi():
+    search = " "
+    if request.form:
+        prediction = request.form.get("eightballz")
+        search = eightball(prediction)
+        if len(prediction) != 0:  # input field has content
+            print("Please enter an input")
+        print(search)
+
+    return render_template("aboutus/briasapi.html", fact=search)
 
 @app.route('/jessie/')
 def jessie():
---
+++ templates/aboutus/briaapi.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def eightball(prediction):
+    url = "https://magic-8-ball1.p.rapidapi.com/my_answer/"
+
+    question = str(prediction)
+    querystring = {"question": question}
+
+    headers = {
+        'x-rapidapi-host': "magic-8-ball1.p.rapidapi.com",
+        'x-rapidapi-key': "435f957ca1msh508be3bb9ed13fap1d89c7jsn47a12462048a"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    return response.text
