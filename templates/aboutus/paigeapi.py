d5daaa07dccf08b77cbb27042162446c8e2e72ff
paigemccartin <89223526+paigemccartin@users.noreply.github.com>
2021-12-09 15:26:58 -0800

    PAIGE CODES LIKE A PRO

--- templates/aboutus/paigeapi.py
+++ templates/aboutus/paigeapi.py
@@ -1,24 +1,17 @@
 import requests
-from flask import Blueprint, render_template
-
-app_starter = Blueprint('aboutus', __name__,
-                        url_prefix='/',
-                        template_folder='templates/aboutus/',
-                        static_folder='static',
-                        static_url_path='assets')
-
-
 
 def population():
     url = "https://world-population.p.rapidapi.com/worldpopulation"
-
     headers = {
-        'x-rapidapi-host': "world-population.p.rapidapi.com",
-        'x-rapidapi-key': "4ead57fd33mshf1561aa23889096p18583ejsncb8b90ef517c"
+        'x-rapidapi-key': "4ead57fd33mshf1561aa23889096p18583ejsncb8b90ef517c",
+        'x-rapidapi-host': "world-population.p.rapidapi.com"
     }
 
     response = requests.request("GET", url, headers=headers)
+    stats = response.json()
+    print(stats)
+    pop = stats['body']['world_population']
+    print(pop)
 
-    factt = response.json()
 
-    return render_template("/paigeapi.html", factt=factt)
+population()
