b098e08f5da9594475866c3731980f97071f1314
parkjessie <jessiepark20040609@gmail.com>
2021-12-16 10:54:23 -0800

    Individual page jessie (API error still present..)

--- templates/aboutus/jessieapi.py
+++ templates/aboutus/jessieapi.py
@@ -1,55 +1,15 @@
-import random
+import requests
 
-from flask import Blueprint, jsonify
+def recipe():
+    url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/images/analyze"
 
-app_api = Blueprint('api', __name__,
-                    url_prefix='/api',
-                    template_folder='templates',
-                    static_folder='static', static_url_path='static/api')
+    querystring = {"imageUrl":"https://spoonacular.com/recipeImages/635350-240x150.jpg"}
 
-fortunes_data = []
-fortune_list = [
-    "A lifetime of happiness lies ahead of you. "
-    "You love challenge.",
-    "Your ability is appreciated.",
-    "Your mind is your greatest asset.",
-    "Your life will get more and more exciting.",
-    "Your mentality is alert, practical, and analytical.",
-    "Your moods signal a period of change.",
-    "A new perspective will come with the new year. "
-    "A golden egg of opportunity falls into your lap this month.",
-    "A gambler not only will lose what he has, but also will lose what he doesn’t have.",
-    "Something is looking up to you. Don't let that person down.",
-    "Run.",
-    "Just have fun.",
-    "Take care of yourself first. Then help others.",
-    "Pay attention to your family, don't take them for granted.",
-    "Purple is your lucky color today.",
-    "Yellow is your color today.",
-    "Be careful of your surroundings.",
-    "Beware.",
-]
+    headers = {
+    'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+    'x-rapidapi-key': "b8ff06ff0emsheb30cad1d33841fp1309bcjsn61e1b6e316de"
+    }
 
+    response = requests.request("GET", url, headers=headers, params=querystring)
 
-def _init_fortunes():
-    item_id = 1
-    for item in fortune_list:
-        fortunes_data.append({"id": item_id, "fortune": item, "haha": 0, "boohoo": 0})
-        item_id += 1
-
-
-@app_api.route('/jessiesapi')
-def fortune():
-    if len(fortunes) == 0:
-        _init_fortunes()
-    return jsonify(random.choice(fortunes_data))
-
-
-def fortunes():
-    if len(fortunes_data) == 0:
-        _init_fortunes()
-    return jsonify(fortunes_data)
-
-
-if __name__ == "__main__":
-    print(random.choice(fortune_list))
+    print(response.text)
