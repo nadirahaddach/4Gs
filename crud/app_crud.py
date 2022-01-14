dae2c2bf33f5cd8e367c7e9f4dce356bc42a22e6
paigemccartin <89223526+paigemccartin@users.noreply.github.com>
2022-01-14 12:24:01 -0800

    Revert "Nav search bar (commit just for progress marking) ignore the error in added crud html and python files"
    This reverts commit 081fd74c6cdb421e6475153908d46e5b5cab61c3.

--- crud/app_crud.py
+++ crud/app_crud.py
@@ -1,19 +1,54 @@
 """control dependencies to support CRUD app routes and APIs"""
 from flask import Blueprint, render_template, request, url_for, redirect, jsonify, make_response
+from flask_restful import Api, Resource
+import requests
 
-from sql import *
+from model import Users
 
 # blueprint defaults https://flask.palletsprojects.com/en/2.0.x/api/#blueprint-objects
 app_crud = Blueprint('crud', __name__,
                      url_prefix='/crud',
-                     template_folder='templates/crud/',
+                     template_folder='/templates/',
                      static_folder='static',
-                     static_url_path='static')
+                     static_url_path='assets')
+
+
+# API generator https://flask-restful.readthedocs.io/en/latest/api.html#id1
+api = Api(app_crud)
 
 """ Application control for CRUD is main focus of this File, key features:
     1.) User table queries
-    2.) app routes for CRUD (Blueprint)
+    2.) app routes (Blueprint)
+    3.) API routes
+    4.) API testing
 """
+
+""" Users table queries"""
+
+
+# User/Users extraction from SQL
+def users_all():
+    """converts Users table into JSON list """
+    return [peep.read() for peep in Users.query.all()]
+
+
+def users_ilike(term):
+    """filter Users table by term into JSON list """
+    term = "%{}%".format(term)  # "ilike" is case insensitive and requires wrapped  %term%
+    table = Users.query.filter((Users.name.ilike(term)))
+    return [peep.read() for peep in table]
+
+
+# User extraction from SQL
+def user_by_id(userid):
+    """finds User in table matching userid """
+    return Users.query.filter_by(userID=userid).first()
+
+
+# User extraction from SQL
+
+
+""" app route section """
 
 
 # Default URL
@@ -30,9 +65,8 @@ def create():
     if request.form:
         po = Users(
             request.form.get("name"),
-            request.form.get("email"),
-            request.form.get("password"),
-            request.form.get("phone")
+            request.form.get("grade"),
+            request.form.get("advice")
         )
         po.create()
     return redirect(url_for('crud.crud'))
@@ -91,3 +125,114 @@ def search_term():
     term = req['term']
     response = make_response(jsonify(users_ilike(term)), 200)
     return response
+
+
+""" API routes section """
+
+
+class UsersAPI:
+    # class for create/post
+    class _Create(Resource):
+        def post(self, name, grade, advice):
+            po = Users(name, grade, advice)
+            person = po.create()
+            if person:
+                return person.read()
+            return {'message': f'Processed {name}, a format error'}, 210
+
+    # class for read/get
+    class _Read(Resource):
+        def get(self):
+            return users_all()
+
+    # class for read/get
+    class _ReadILike(Resource):
+        def get(self, term):
+            return users_ilike(term)
+
+    # class for update/put
+    class _Update(Resource):
+        def put(self, name):
+            po.update(name)
+            return po.read()
+
+    class _UpdateAll(Resource):
+        def put(self, name, password, grade, advice):
+            po.update(name, password, grade, advice)
+            return po.read()
+
+    # class for delete
+    class _Delete(Resource):
+        def delete(self, userid):
+            po = user_by_id(userid)
+            if po is None:
+                return {'message': f"{userid} is not found"}, 210
+            data = po.read()
+            po.delete()
+            return data
+
+    # building RESTapi resource
+    api.add_resource(_Create, '/create/<string:name>/<string:grade>/<string:advice>')
+    api.add_resource(_Read, '/read/')
+    api.add_resource(_ReadILike, '/read/ilike/<string:term>')
+    api.add_resource(_Update, '/update/<string:name>')
+    api.add_resource(_UpdateAll, '/update/<string:name>/<string:grade>/<string:advice>')
+    api.add_resource(_Delete, '/delete/<int:userid>')
+
+
+""" API testing section """
+
+
+def api_tester():
+    # local host URL for model
+    url = 'http://localhost:5222/crud'
+
+    # test conditions
+    API = 0
+    METHOD = 1
+    tests = [
+        ['/create/Wilma Flintstone/wilma@bedrock.org/123wifli/0001112222', "post"],
+        ['/create/Fred Flintstone/fred@bedrock.org/123wifli/0001112222', "post"],
+        ['/read/', "get"],
+        ['/read/ilike/John', "get"],
+        ['/read/ilike/com', "get"],
+        ['/update/wilma@bedrock.org/Wilma S Flintstone/123wsfli/0001112229', "put"],
+        ['/update/wilma@bedrock.org/Wilma Slaghoople Flintstone', "put"],
+        ['/delete/4', "delete"],
+        ['/delete/5', "delete"],
+    ]
+
+    # loop through each test condition and provide feedback
+    for test in tests:
+        print()
+        print(f"({test[METHOD]}, {url + test[API]})")
+        if test[METHOD] == 'get':
+            response = requests.get(url + test[API])
+        elif test[METHOD] == 'post':
+            response = requests.post(url + test[API])
+        elif test[METHOD] == 'put':
+            response = requests.put(url + test[API])
+        elif test[METHOD] == 'delete':
+            response = requests.delete(url + test[API])
+        else:
+            print("unknown RESTapi method")
+            continue
+
+        print(response)
+        try:
+            print(response.json())
+        except:
+            print("unknown error")
+
+
+def api_printer():
+    print()
+    print("Users table")
+    for user in users_all():
+        print(user)
+
+
+"""validating api's requires server to be running"""
+if __name__ == "__main__":
+    api_tester()
+    api_printer()
